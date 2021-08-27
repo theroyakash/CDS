02437180278fd775f73a29693646c5fe7944cc59
theroyakash <royakashappleid@icloud.com>
2021-08-27 16:17:25 +0530

    Update tree.c

--- tree/tree.c
+++ tree/tree.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 struct TreeNode {
-    char data;
-    struct TreeNode *left, *right;
+	char data;
+	struct TreeNode *left, *right;
 };
 
 typedef struct TreeNode TreeNode;
@@ -10,72 +10,72 @@ typedef struct TreeNode TreeNode;
 
 void inOrderTraversal(TreeNode *nodePointer){
 
-    if (nodePointer != NULL) {
-        inOrderTraversal(nodePointer->left);
-        printf("%c", nodePointer->data);
-        inOrderTraversal(nodePointer->right);
-    }
+	if (nodePointer != NULL) {
+		inOrderTraversal(nodePointer->left);
+		printf("%c", nodePointer->data);
+		inOrderTraversal(nodePointer->right);
+	}
 }
 
 void preOrderTraversal(TreeNode *nodePointer){
 
-    if (nodePointer != NULL) {
-        printf("%c", nodePointer->data);
-        preOrderTraversal(nodePointer->left);
-        preOrderTraversal(nodePointer->right);
-    }
+	if (nodePointer != NULL) {
+		printf("%c", nodePointer->data);
+		preOrderTraversal(nodePointer->left);
+		preOrderTraversal(nodePointer->right);
+	}
 }
 
 void postOrderTraversal(TreeNode *nodePointer){
 
-    if (nodePointer != NULL) {
-        postOrderTraversal(nodePointer->left);
-        postOrderTraversal(nodePointer->right);
+	if (nodePointer != NULL) {
+		postOrderTraversal(nodePointer->left);
+		postOrderTraversal(nodePointer->right);
 
-        printf("%c", nodePointer->data);
-    }
+		printf("%c", nodePointer->data);
+	}
 }
 
 // Double order traversal: print every node twice.
 void doubleOrderTraversal(TreeNode *basePointer){
-    if (basePointer != NULL) {
+	if (basePointer != NULL) {
 
-        printf("%c", basePointer -> data);
-        doubleOrderTraversal(basePointer -> left);
-        printf("%c", basePointer -> data);
-        doubleOrderTraversal(basePointer -> right);
-    }
+		printf("%c", basePointer -> data);
+		doubleOrderTraversal(basePointer -> left);
+		printf("%c", basePointer -> data);
+		doubleOrderTraversal(basePointer -> right);
+	}
 }
 
 // count and return the number of nodes in the BT
 
 long int countNodes(TreeNode *head){
-    if (head != NULL) {
-        return (1 + countNodes(head -> left) + countNodes(head -> right));
-    } else {
-        return 0;
-    }
+	if (head != NULL) {
+		return (1 + countNodes(head -> left) + countNodes(head -> right));
+	} else {
+		return 0;
+	}
 }
 
 // Count number of leafs in BT
 
 long int leafs(TreeNode *head){
 
-    if (head) {
-        if (head -> left == NULL && head -> right == NULL) {
-            return 1;
-        } else {
-            return leafs(head -> left) + leafs(head -> right);
-        }
-    }
+	if (head) {
+		if (head -> left == NULL && head -> right == NULL) {
+			return 1;
+		} else {
+			return leafs(head -> left) + leafs(head -> right);
+		}
+	}
 
-    else{
-        return 0;
-    }
+	else{
+		return 0;
+	}
 }
 
 long int max(long int a, long int b){
-    return (a>b)? a: b;
+	return (a>b)? a: b;
 }
 
 
@@ -83,15 +83,15 @@ long int max(long int a, long int b){
 
 long int treeDepth(TreeNode *head){
 
-    if (head) {
-        if (head -> right == NULL && head -> left == NULL) {
-            return 0;
-        }else{
-            return 1+ max(treeDepth(head -> left), treeDepth(head -> right));
-        }
-    }else{
-        return 0;
-    }
+	if (head) {
+		if (head -> right == NULL && head -> left == NULL) {
+			return 0;
+		}else{
+			return 1+ max(treeDepth(head -> left), treeDepth(head -> right));
+		}
+	}else{
+		return 0;
+	}
 
 }
 
@@ -100,17 +100,17 @@ long int treeDepth(TreeNode *head){
 
 long int countFullNodes(TreeNode *head){
 
-    if (head){
-        if (head -> right == NULL && head -> left == NULL) {
-            return 0;
-        }else{
-            return countFullNodes(head -> left) + countFullNodes(head -> right) + ((head -> left && head -> right)? 1: 0);
-        }
-    }
+	if (head){
+		if (head -> right == NULL && head -> left == NULL) {
+			return 0;
+		}else{
+			return countFullNodes(head -> left) + countFullNodes(head -> right) + ((head -> left && head -> right)? 1: 0);
+		}
+	}
 
-    else{
-        return 0;
-    }
+	else{
+		return 0;
+	}
 }
 
 
@@ -118,43 +118,43 @@ long int countFullNodes(TreeNode *head){
 int main(){
 
 
-    TreeNode n1, n2, n3;
+	TreeNode n1, n2, n3;
 
-    n1.data = 'A';
-    n2.data = 'B';
-    n3.data = 'C';
+	n1.data = 'A';
+	n2.data = 'B';
+	n3.data = 'C';
 
-    TreeNode n4 = {'D', NULL, NULL};
-    TreeNode n5 = {'E', NULL, NULL};
+	TreeNode n4 = {'D', NULL, NULL};
+	TreeNode n5 = {'E', NULL, NULL};
 
-    n1.left = &n2;
-    n1.right = &n3;
+	n1.left = &n2;
+	n1.right = &n3;
 
-    n2.left = &n5;
-    n2.right = &n4;
+	n2.left = &n5;
+	n2.right = &n4;
 
-    n3.left = NULL;
-    n3.right = NULL;
+	n3.left = NULL;
+	n3.right = NULL;
 
-    printf("In Order: ");
-    inOrderTraversal(&n1);
-    printf("\n");
-    printf("Pre Order: ");
-    preOrderTraversal(&n1);
-    printf("\n");
-    printf("Post Order: ");
-    postOrderTraversal(&n1);
-    printf("\n");
-    printf("Double Order: ");
-    doubleOrderTraversal(&n1);
+	printf("In Order: ");
+	inOrderTraversal(&n1);
+	printf("\n");
+	printf("Pre Order: ");
+	preOrderTraversal(&n1);
+	printf("\n");
+	printf("Post Order: ");
+	postOrderTraversal(&n1);
+	printf("\n");
+	printf("Double Order: ");
+	doubleOrderTraversal(&n1);
 
 
-    printf("\nNumber of nodes in the tree is %ld\n", countNodes(&n1));
+	printf("\nNumber of nodes in the tree is %ld\n", countNodes(&n1));
 
-    printf("Number of Leafs in the tree is %ld\n", leafs(&n1));
-    printf("Number of full nodes in the tree is %ld\n", countFullNodes(&n1));
+	printf("Number of Leafs in the tree is %ld\n", leafs(&n1));
+	printf("Number of full nodes in the tree is %ld\n", countFullNodes(&n1));
 
-    printf("Tree Depth is %ld\n", treeDepth(&n1));
+	printf("Tree Depth is %ld\n", treeDepth(&n1));
 
-    return 0;
+	return 0;
 }
