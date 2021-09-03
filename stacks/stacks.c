3a1b2f87f9c64695d8762cfcac3cd83f4d6e3253
theroyakash <royakashappleid@icloud.com>
2021-09-03 12:20:52 +0530

    stacks started

---
+++ stacks/stacks.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "stacks.h"
+
+/*
+Basic Operations on a Stack
+    - push(): Add an element to the top of a stack
+    - pop(): Remove an element from the top of a stack
+    - isEmpty(): Check if the stack is empty
+    - isFull(): Check if the stack is full
+    - peek(): Get the value of the top element without removing it
+*/
+
---
+++ stacks/stacks.h
@@ -0,0 +1,16 @@
+/** @file tree.h
+ *  @brief Function prototypes for stacks implemented with arrays.
+ *
+ *  This contains the prototypes for for stacks implemented with arrays,
+ *  every utility function for a stacks you will ever need.
+ *
+ *  @author theroyakash
+ *  @bug No known bugs.
+ */
+
+#ifndef STACKS
+#define STACKS
+
+
+
+#endif /* STACKS */
