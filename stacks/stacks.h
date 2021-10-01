9c678bfc9bd474bd4a12a61c8399fbf8edb8c362
theroyakash <royakashappleid@icloud.com>
2021-10-01 23:26:54 +0530

    Stack header files added

--- stacks/stacks.h
+++ stacks/stacks.h
@@ -11,6 +11,31 @@
 #ifndef STACKS
 #define STACKS
 
+#include <stdio.h>
+#include <stdbool.h>
 
+struct Stack{
+    int size;
+    // Stack Bucket is the place where stack elements are stored
+    int *stackBucket;
+};
+
+/*
+Basic Operations on a Stack
+    - push(): Add an element to the top of a stack
+    - pop(): Remove an element from the top of a stack
+    - isEmpty(): Check if the stack is empty
+    - isFull(): Check if the stack is full
+    - peek(): Get the value of the top element without removing it
+*/
+
+
+typedef struct Stack Stack;
+bool isEmpty(Stack *stack);
+bool isFull(Stack *stack);
+
+void push(int element);
+int pop(Stack *stack);
+int peek(Stack *stack);
 
 #endif /* STACKS */
